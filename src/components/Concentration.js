ab0482258bd5eb98778703ba2238b6156dbc9016
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-05 15:48:16 -0400

    format file

--- src/components/Concentration.js
+++ src/components/Concentration.js
@@ -1,102 +1,134 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
 const styles = {
-  fontFamily: 'sans-serif',
-  textAlign: 'center'
+  fontFamily: "sans-serif",
+  textAlign: "center"
 };
 
 class Concentration extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      flippedOneType: '', 
-      flippedOneID: '',
-      flippedTwoType: '', 
-      flippedTwoID: '', 
+      flippedOneType: "",
+      flippedOneID: "",
+      flippedTwoType: "",
+      flippedTwoID: "",
       gameWon: 0,
-      1: '', 
-      2: '', 
-      3: '', 
-      4: '', 
-      5: '', 
-      6: '', 
-      7: '', 
-      8: '', 
-      9: '', 
-      10: '', 
-      11: '', 
-      12: '', 
-      13: '', 
-      14: '', 
-      15: '', 
-      16: '', 
-      17: '', 
-      18: '', 
-      19: '', 
-      20: '', 
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+      9: "",
+      10: "",
+      11: "",
+      12: "",
+      13: "",
+      14: "",
+      15: "",
+      16: "",
+      17: "",
+      18: "",
+      19: "",
+      20: ""
     };
   }
 
-  handleClick = (e) => { 
-    const { flippedOneType, flippedOneID, flippedTwoID, flippedTwoType } = this.state
-    console.log('e.target', e.target)
-    const image = e.target
-    const imageID = image.id
-    if(flippedOneID && flippedTwoID){
-console.log('first ran')
-    }
-    else if(!flippedOneID){
-      console.log('flippedONe is undefined')
+  handleClick = e => {
+    const {
+      flippedOneType,
+      flippedOneID,
+      flippedTwoID,
+      flippedTwoType
+    } = this.state;
+    console.log("e.target", e.target);
+    const image = e.target;
+    const imageID = image.id;
+    if (flippedOneID && flippedTwoID) {
+      console.log("first ran");
+    } else if (!flippedOneID) {
+      console.log("flippedONe is undefined");
       this.setState({
-        [imageID]: 'flipped', 
-        flippedOneID: imageID, 
+        [imageID]: "flipped",
+        flippedOneID: imageID,
         flippedOneType: image.name
-      })
-    } else if(!flippedTwoID){
-      console.log('flipped two undefined')
-      this.setState({
-        [imageID]: 'flipped', 
-        flippedTwoID: imageID, 
-        flippedTwoType: image.name
-      }, () => { 
-        if(this.state.flippedTwoType === this.state.flippedOneType) { 
-          this.setState({
-            flippedOneID: '', 
-            flippedOneType: '', 
-            flippedTwoID: '', 
-            flippedTwoType: '', 
-          })
-        }else { 
-  console.log('images not equal')
-  setTimeout(function(){         
-    this.setState({
-    [imageID]: '', 
-    flippedOneID: '', 
-    flippedOneType: '', 
-    [flippedOneID]: '', 
-    flippedTwoID: '', 
-    flippedTwoType: '', 
-    [flippedTwoID]: ''
-  })}.bind(this), 2000);
+      });
+    } else if (!flippedTwoID) {
+      console.log("flipped two undefined");
+      this.setState(
+        {
+          [imageID]: "flipped",
+          flippedTwoID: imageID,
+          flippedTwoType: image.name
+        },
+        () => {
+          if (this.state.flippedTwoType === this.state.flippedOneType) {
+            this.setState({
+              flippedOneID: "",
+              flippedOneType: "",
+              flippedTwoID: "",
+              flippedTwoType: ""
+            });
+          } else {
+            console.log("images not equal");
+            setTimeout(
+              function() {
+                this.setState({
+                  [imageID]: "",
+                  flippedOneID: "",
+                  flippedOneType: "",
+                  [flippedOneID]: "",
+                  flippedTwoID: "",
+                  flippedTwoType: "",
+                  [flippedTwoID]: ""
+                });
+              }.bind(this),
+              2000
+            );
+          }
         }
-      })
+      );
     }
-  }
+  };
 
   render() {
-    console.log('this.state', this.state)
+    console.log("this.state", this.state);
     return (
       <div style={styles}>
         <h1>Concentration</h1>
-        
-        {(this.props.iconArr).map(value => (
-          (this.state[value.id] !== 'flipped') ? <img className="flipped" onClick={this.handleClick} id={value.id} name={value.type} width='82px' height='82px' src={`https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Black_card.svg/788px-Black_card.svg.png`}/>
-          : <img className="image" onClick={this.handleClick} id={value.id} name={value.type} src={`https://raw.githubusercontent.com/C4Q/AC_4_Web/master/units/react/exercises/objects_and_arrays/cards/${value.type}.png`}/>
-        ))}
+        {}
+
+        {this.props.iconArr.map(
+          value =>
+            this.state[value.id] !== "flipped" ? (
+              <img
+                className="flipped"
+                onClick={this.handleClick}
+                id={value.id}
+                name={value.type}
+                width="82px"
+                height="82px"
+                src={`https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Black_card.svg/788px-Black_card.svg.png`}
+              />
+            ) : (
+              <img
+                className="image"
+                onClick={this.handleClick}
+                id={value.id}
+                name={value.type}
+                src={`https://raw.githubusercontent.com/C4Q/AC_4_Web/master/units/react/exercises/objects_and_arrays/cards/${
+                  value.type
+                }.png`}
+              />
+            )
+        )}
       </div>
     );
   }
 }
 
-export default Concentration; 
+export default Concentration;
