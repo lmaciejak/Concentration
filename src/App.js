71a05dfc14b7948a4c634c9e9a994292b9dff933
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-05 13:47:57 -0400

    add id to card

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Concentration from './components/Concentration';
 
 class App extends Component {
 
-  imagesArr = [{id: 1, type: 'apple'}, {id: 2, type: 'camera'}, {id: 3, type:'clover'}, {id: 4, type:'coffee'}, {id: 5, type:'heart'}, {id: 6, type:'key'}, {id: 7, type:'paw'}, {id: 8, type:'smiley'}, 
-  {id: 9, type:'snowflake'}, {id: 10, type:'star'}, {id: 11, type:'apple'}, {id: 12, type:'camera'}, {id: 13, type:'clover'}, {id: 14, type:'coffee'}, {id: 15, type:'heart'}, {id: 16, type:'key'}, 
+  imagesArr = [{id: 1, type: 'apple'}, {id: 2, type:'apple'}, {id: 3, type: 'camera'}, {id: 4, type:'camera'}, 
+  {id: 4, type:'clover'}, {id: 5, type:'clover'}, {id: 6, type:'coffee'},{id: 7, type:'coffee'}, 
+  {id: 8, type:'heart'}, {id: 9, type:'heart'}, {id: 10, type:'key'}, {id: 11, type:'key'}, {id: 7, type:'paw'}, {id: 8, type:'smiley'}, 
+  {id: 9, type:'snowflake'}, {id: 10, type:'star'},  
   {id: 17, type:'paw'}, {id: 18, type:'smiley'}, {id: 19, type:'snowflake'}, {id: 20, type:'star'}]
 
   shuffleArray(array) {
--- src/components/Concentration.js
+++ src/components/Concentration.js
@@ -54,29 +54,36 @@ class Concentration extends React.Component {
     const { flippedOneType } = this.state
     console.log('e.target', e.target)
     const image = e.target
-    if(!flippedOneType[0]){
-      console.log('first if ran')
-      e.target.style.backgroundColor = "transparent"
-      this.setState({
-        flippedOneType: e.target.name, 
-        flippedOneID: e.target.id
-      })
-    } else {
-      const image = e.target
-      console.log('flippedonetarget', this.state.flippedOneID)
-      e.target.style.backgroundColor = "transparent"
-      var elem = document.getElementById(`${this.state.flippedOneID}`)
-      if(e.target.name !== flippedOneType){
-        setTimeout(function () { image.style.backgroundColor = "black" 
-        elem.style.backgroundColor = "black"; }, 2000)
-        console.log('this ran')
-      }else { 
-        this.setState({
-          flippedOneType: '',
-          flippedOneID: '',
-        })
-      }
-    }
+    const imageID = image.id
+    this.setState({
+      [imageID]: 'flipped' 
+    })
+    // if(!flippedOneType[0]){
+    //   console.log('first if ran')
+    //   this.setState({
+    //     flippedOneType: image.name, 
+    //     flippedOneID: image.id,
+    //     [imageID]: 'flipped' 
+    //   })
+    // } else {
+    //   const image = e.target
+    //   console.log('flippedonetarget', this.state.flippedOneID)
+    //   if(e.target.name !== flippedOneType){
+    //     this.setState({
+    //       flippedOneType: '', 
+    //       flippedOneID: '',
+    //       [imageID]: '',
+
+    //     })
+    //     console.log('this ran')
+    //   }else { 
+    //     this.setState({
+    //       flippedOneType: '',
+    //       flippedOneID: '',
+    //     })
+    //   }
+    // }
+    
     // if (image.style.backgroundColor === "transparent"){ 
     //   image.style.backgroundColor = "black"
     // } else { 
@@ -88,12 +95,15 @@ class Concentration extends React.Component {
   render() {
     console.log('flipped one', this.state.flippedOneType)
     console.log('flipped one id', this.state.flippedOneID)
+    console.log('rerendered')
+    console.log('props', this.state[this.props.iconArr[0].id])
     return (
       <div style={styles}>
         <h1>Concentration</h1>
         
         {(this.props.iconArr).map(value => (
-          <img className="image" onClick={this.handleClick} id={value.id} name={value.type} src={`https://raw.githubusercontent.com/C4Q/AC_4_Web/master/units/react/exercises/objects_and_arrays/cards/${value.type}.png`}/>
+          (this.state[value.id] !== 'flipped') ? <img className="flipped" onClick={this.handleClick} id={value.id} name={value.type} width='82px' height='82px' src={`https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Black_card.svg/788px-Black_card.svg.png`}/>
+          : <img className="image" onClick={this.handleClick} id={value.id} name={value.type} src={`https://raw.githubusercontent.com/C4Q/AC_4_Web/master/units/react/exercises/objects_and_arrays/cards/${value.type}.png`}/>
         ))}
       </div>
     );
