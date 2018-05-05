cd2c7a2708fb616b8a90e34b6ca3763165f2830b
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2018-05-05 14:01:51 -0400

    update imagesArr

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Concentration from './components/Concentration';
 class App extends Component {
 
   imagesArr = [{id: 1, type: 'apple'}, {id: 2, type:'apple'}, {id: 3, type: 'camera'}, {id: 4, type:'camera'}, 
-  {id: 4, type:'clover'}, {id: 5, type:'clover'}, {id: 6, type:'coffee'},{id: 7, type:'coffee'}, 
-  {id: 8, type:'heart'}, {id: 9, type:'heart'}, {id: 10, type:'key'}, {id: 11, type:'key'}, {id: 7, type:'paw'}, {id: 8, type:'smiley'}, 
-  {id: 9, type:'snowflake'}, {id: 10, type:'star'},  
-  {id: 17, type:'paw'}, {id: 18, type:'smiley'}, {id: 19, type:'snowflake'}, {id: 20, type:'star'}]
+  {id: 5, type:'clover'}, {id: 6, type:'clover'}, {id: 7, type:'coffee'},{id: 8, type:'coffee'}, 
+  {id: 9, type:'heart'}, {id: 10, type:'heart'}, {id: 11, type:'key'}, {id: 12, type:'key'}, 
+  {id: 13, type:'paw'},   {id: 14, type:'paw'},{id: 15, type:'smiley'},  {id: 16, type:'smiley'},
+  {id: 17, type:'snowflake'},  {id: 18, type:'snowflake'},{id: 19, type:'star'},{id: 20, type:'star'}]
 
   shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
